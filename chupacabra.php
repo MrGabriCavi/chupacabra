7f660faa420a9ae1f1b2cbbd944655934a4eca56
MrGabriCavi <gabri.accounts@gmail.com>
2014-07-07 11:02:28 +0200

    Si comincia!!!

---
+++ chupacabra.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by Gabriele Cavigiolo.
+ * Mail: gabri.cavi@gmail.com
+ * Date: 07/07/14
+ * Time: 10:50
+ */
+
+class chupacabra{
+
+    /**
+     * Driver PDO for db
+     * @var string
+     */
+    private static $dbdriver = "mysql";
+
+    /**
+     * Istance of PDO
+     * @var
+     */
+    private $dbconn;
+
+    protected function openConnection(){
+
+    }
+
+}
---
+++ run.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by Gabriele Cavigiolo.
+ * Mail: gabri.cavi@gmail.com
+ * Date: 07/07/14
+ * Time: 10:51
+ */
+
+$chupa = new chupacabra();
+
